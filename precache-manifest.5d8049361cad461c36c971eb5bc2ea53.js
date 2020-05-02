bd9fc98b47ace1b88fe80cea5aa249a3fdb693ef
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-02 16:32:04 -0300

    Updates

---
+++ precache-manifest.5d8049361cad461c36c971eb5bc2ea53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5ca8400d661b018da48a647d35d17c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5cf89bb5eb4170c458a3",
+    "url": "./static/css/main.df9fdef5.chunk.css"
+  },
+  {
+    "revision": "9e11a2693a2fce3c7239",
+    "url": "./static/js/2.090a20f8.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "./static/js/2.090a20f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf89bb5eb4170c458a3",
+    "url": "./static/js/main.addcb61e.chunk.js"
+  },
+  {
+    "revision": "defb46d4e4da00488651",
+    "url": "./static/js/runtime-main.a90ece73.js"
+  },
+  {
+    "revision": "a8c9cd435a91f2ef85cf0583d6c70c51",
+    "url": "./static/media/arrow.a8c9cd43.svg"
+  },
+  {
+    "revision": "bccb4b871a0344e9187c28cc8e97eceb",
+    "url": "./static/media/bg.bccb4b87.png"
+  }
+]);
