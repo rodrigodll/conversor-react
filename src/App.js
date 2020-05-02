79eb77540c242e82b5c2de47ffc23c4c4becfafd
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-01 23:44:52 -0300

    stylizes header and footer

--- src/App.js
+++ src/App.js
@@ -10,17 +10,53 @@ function App() {
   return (
     <div className="App">
 
-      <div class="container">
-        <div class="row">      
-          <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
+        <div className="container">
+          <div className="row justify-content-md-center">
+            <div className="col-12">
+              <img src={logo} className="header__logo" alt="logo" />
+            </div>
+            <div className="col-12">
+              <nav className="header__menu">
+                <ul>
+                  <li>
+                    <a href="/" className="active">Exchange</a>
+                  </li>
+                  <li>
+                    <a href="/">About</a>
+                  </li>
+                  <li>
+                    <a href="/">Contact</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
         </div>
-      </div>
+      </header>
       
-      <div class="container">
-        <div class="row">      
-          <Conversor moedaA="BTC" moedaB="BRL"></Conversor>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="heading">
+              My first APP built on <a href="https://pt-br.reactjs.org/" target="_blank">React JS</a>, a currency exchange solution created with the goal of personal development.
+            </h3>
+          </div>
+          <Conversor/>
         </div>
       </div>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <p className="">
+                Rodrigodll <a href="http://github.com/rodrigodll/" target="_blank">github</a> © 2020
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
